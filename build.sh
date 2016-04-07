812c7d937db93302023be7c9037acc6d40f20bfe
Toto Castaldi <toto.castaldi@gmail.com>
2016-04-07 14:11:50 +0200

    Enaip Giacinti WIP - 20160404

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ python convertToAsc.py templates/Electronicdrums/Electronicdrums
 python convertToAsc.py templates/TapTheMole/TapTheMole
 python convertToAsc.py templates/ClientServerString/ClientServerString
 python convertToAsc.py templates/TapTheMoleOnline/TapTheMoleOnline
+python convertToAsc.py templates/EnaipGiacintiWip20160404/EnaipGiacintiWip20160404
 
 rm -f download.zip
 
