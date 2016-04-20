5f01637c4912c877e2b09561c0c1ef17cc9d43db
toto castaldi <toto.castaldi@gmail.com>
2016-04-20 22:35:55 +0200

    TapTheMoleOnlineSimple

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ set -e
 python convertToAsc.py templates/Electronicdrums/Electronicdrums
 python convertToAsc.py templates/TapTheMole/TapTheMole
 python convertToAsc.py templates/ClientServerString/ClientServerString
+python convertToAsc.py templates/TapTheMoleOnlineSimple/TapTheMoleOnlineSimple
 python convertToAsc.py templates/TapTheMoleOnline/TapTheMoleOnline
 python convertToAsc.py templates/EnaipGiacintiWip20160404/EnaipGiacintiWip20160404
 python convertToAsc.py templates/EnaipGiacintiWip20160407/EnaipGiacintiWip20160407
